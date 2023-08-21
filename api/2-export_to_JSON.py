2726a725dbd7ee324bf5da2c3cde948a9970c107
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-21 06:45:51 -0700

    Pycode

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -13,16 +13,15 @@ if __name__ == "__main__":
         f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     employee_todos = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    
+
     # Add tasks to list
     for task in employee_todos.json():
-        task_dict = {"task": task["title"], "completed": task["completed"], "username": employee.json()["username"]}
+        task_dict = {"task": task["title"], "completed": task["completed"],
+                     "username": employee.json()["username"]}
         task_list.append(task_dict)
-    
+
     # Create dict with ID as Key and list as Value
     employee_task_dict = {f"{employee_id}": task_list}
 
     with open(f'{argv[1]}.json', 'w', encoding='utf-8') as file:
         json.dump(employee_task_dict, file)
-
-    
